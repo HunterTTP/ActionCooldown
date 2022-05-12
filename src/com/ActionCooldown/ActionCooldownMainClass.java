171e64d9577b7737145b19ab8a5323d22d63389f
BuildTools <unconfigured@null.spigotmc.org>
2022-05-11 22:53:31 -0400

    First commit; basic structure.

---
+++ src/com/ActionCooldown/ActionCooldownMainClass.java
@@ -0,0 +1,17 @@
+package com.ActionCooldown;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ActionCooldownMainClass extends JavaPlugin {
+    
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
