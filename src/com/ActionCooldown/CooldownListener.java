c1d18d55e4165f2cbd6b1964d1fd578252e6bc3d
HunterTTP <HunterTTP@gmail.com>
2022-05-15 19:51:34 -0400

    Action Limit logic implemented

--- src/com/ActionCooldown/CooldownListener.java
+++ src/com/ActionCooldown/CooldownListener.java
@@ -11,10 +11,12 @@ import org.bukkit.event.block.BlockPlaceEvent;
 public class CooldownListener implements Listener{
 	
 	private final CooldownManager cooldownManager = new CooldownManager();
+	public long BlockPlaceEventCounter = 0;
+	public long BlockBreakEventCounter = 0;
 	
     @EventHandler
     public void onPlace(BlockPlaceEvent event) {
-			
+    	
 			//Check if enabled
 			if(CooldownSettings.block_place_cd_enabeled == "true") {
 				
@@ -22,24 +24,34 @@ public class CooldownListener implements Listener{
 				String uniqueid = player.getUniqueId().toString();
 				String eventname = event.getEventName();
 				String playerevent = uniqueid + eventname;
-			
-	    		//Get the amount of milliseconds that have passed since the feature was last used.
-	            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(playerevent);
-	            
-	            if(TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownSettings.block_place_duration){
-	            	
-	                player.sendMessage(ChatColor.GREEN + "A block has been placed!");
-	                cooldownManager.setCooldown(playerevent, System.currentTimeMillis());
-	            
-	            }else{
-	            	
-	            	event.setCancelled(true);
-	                player.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownSettings.block_place_duration) + " seconds before " + playerevent + " can place blocks again"); 
+				
+				if(BlockPlaceEventCounter < CooldownSettings.block_place_cd_limit) {
+					
+					BlockPlaceEventCounter++;
+					
+					cooldownManager.setCooldown(playerevent, System.currentTimeMillis());
+					
+				}else{
+				
+
+		            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(playerevent);
+		            
+		            if(TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownSettings.block_place_cd_duration){
+		            	
+						cooldownManager.setCooldown(playerevent, System.currentTimeMillis());
+						
+						BlockPlaceEventCounter = 1;
+		            
+		            }else{
+		            	
+		            	event.setCancelled(true);
+		            	
+		                player.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownSettings.block_place_cd_duration) + " seconds before you can do that again");
 	                
-	            }
-            }
-            
-    }
+		            }
+				}
+			}     
+    	}
     
     
     @EventHandler
@@ -47,28 +59,36 @@ public class CooldownListener implements Listener{
     	
 		//Check if enabled
 		if(CooldownSettings.block_break_cd_enabeled == "true") {
-    		
+			
 			Player player = event.getPlayer();
 			String uniqueid = player.getUniqueId().toString();
 			String eventname = event.getEventName();
 			String playerevent = uniqueid + eventname;
-            
-    		//Get the amount of milliseconds that have passed since the feature was last used.
-            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(playerevent);
-            
-            if(TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownSettings.block_break_duration){
-            	
-                player.sendMessage(ChatColor.GREEN + "A block has been placed!");
-                cooldownManager.setCooldown(playerevent, System.currentTimeMillis());
-            
-            }else{
-            	
-            	event.setCancelled(true);
-                player.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownSettings.block_break_duration) + " seconds before " + playerevent + " can place blocks again"); 
-
-            }
-		}
-            
-    }
-    
+			
+			if(BlockBreakEventCounter < CooldownSettings.block_break_cd_limit) {
+				
+				BlockBreakEventCounter++;
+				
+				cooldownManager.setCooldown(playerevent, System.currentTimeMillis());
+				
+			}else{
+    		
+	            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(playerevent);
+	            
+	            if(TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownSettings.block_break_cd_duration){
+	            	
+					cooldownManager.setCooldown(playerevent, System.currentTimeMillis());
+					
+					BlockBreakEventCounter = 1;
+	            
+	            }else{
+	            	
+	            	event.setCancelled(true);
+	            	
+	                player.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownSettings.block_break_cd_duration) + " seconds before you can do that again");
+	
+	            }
+			}	
+		}     
+    } 
 }
--- src/com/ActionCooldown/CooldownSettings.java
+++ src/com/ActionCooldown/CooldownSettings.java
@@ -5,9 +5,11 @@ public class CooldownSettings {
 	private static MainClass mainClass = MainClass.getInstance();
 	
 	public static String block_place_cd_enabeled = mainClass.getConfig().getString("block-place-cooldown-enabled");
-	public static long block_place_duration = Long.parseLong(mainClass.getConfig().getString("block-place-cooldown-duration"));
+	public static long block_place_cd_limit = Long.parseLong(mainClass.getConfig().getString("block-place-cooldown-actionlimit"));
+	public static long block_place_cd_duration = Long.parseLong(mainClass.getConfig().getString("block-place-cooldown-duration"));
 	
 	public static String block_break_cd_enabeled = mainClass.getConfig().getString("block-break-cooldown-enabled");
-	public static long block_break_duration = Long.parseLong(mainClass.getConfig().getString("block-break-cooldown-duration"));
+	public static long block_break_cd_limit = Long.parseLong(mainClass.getConfig().getString("block-break-cooldown-actionlimit"));
+	public static long block_break_cd_duration = Long.parseLong(mainClass.getConfig().getString("block-break-cooldown-duration"));
 	
 }
