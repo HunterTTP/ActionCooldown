44e6bbfef8029c050b7801dde20385830497a3c1
HunterTTP <HunterTTP@gmail.com>
2022-05-14 20:33:12 -0400

    Now players can place a single block and then must wait some time before they can place another. Achieved through CooldownListener.

--- src/com/ActionCooldown/CooldownListener.java
+++ src/com/ActionCooldown/CooldownListener.java
@@ -1,14 +1,41 @@
 package com.ActionCooldown;
 
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class CooldownListener implements Listener{
 
+	private final CooldownManager cooldownManager = new CooldownManager();
+	
     @EventHandler
+    //public void onPlace(BlockPlaceEvent event, CommandSender sender, Command command, String label, String[] args) {
     public void onPlace(BlockPlaceEvent event) {
-    	event.setCancelled(true);
-    	}
+    	
+    		
+			Player p = event.getPlayer();
+            
+    		//Get the amount of milliseconds that have passed since the feature was last used.
+            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(p.getUniqueId());
+            
+            if(TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownManager.DEFAULT_COOLDOWN){
+            	
+                p.sendMessage(ChatColor.GREEN + "A block has been placed!");
+                cooldownManager.setCooldown(p.getUniqueId(), System.currentTimeMillis());
+            
+            }else{
+            	
+            	event.setCancelled(true);
+                p.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownManager.DEFAULT_COOLDOWN) + " seconds before you can use this feature again."); 
+                
+            }
+            
+    }
     
 }
