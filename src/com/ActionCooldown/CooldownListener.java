56694e4d18d6d7f219bef081cfbc29e8cde103d2
HunterTTP <HunterTTP@gmail.com>
2022-05-15 12:46:44 -0400

    Updated listener logic to be unique

--- src/com/ActionCooldown/CooldownListener.java
+++ src/com/ActionCooldown/CooldownListener.java
@@ -1,7 +1,6 @@
 package com.ActionCooldown;
 
 import java.util.concurrent.TimeUnit;
-
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,23 +12,26 @@ public class CooldownListener implements Listener{
 	private final CooldownManager cooldownManager = new CooldownManager();
 	
     @EventHandler
-    //public void onPlace(BlockPlaceEvent event, CommandSender sender, Command command, String label, String[] args) {
     public void onPlace(BlockPlaceEvent event) {
     		
 			Player p = event.getPlayer();
+			String pn = p.getName();
+			//String u = p.getUniqueId().toString();
+			String e = event.getEventName();
+			String ue = pn + e;
             
     		//Get the amount of milliseconds that have passed since the feature was last used.
-            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(p.getUniqueId());
+            long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(ue);
             
             if(TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownManager.DEFAULT_COOLDOWN){
             	
                 p.sendMessage(ChatColor.GREEN + "A block has been placed!");
-                cooldownManager.setCooldown(p.getUniqueId(), System.currentTimeMillis());
+                cooldownManager.setCooldown(ue, System.currentTimeMillis());
             
             }else{
             	
             	event.setCancelled(true);
-                p.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownManager.DEFAULT_COOLDOWN) + " seconds before you can place blocks again"); 
+                p.sendMessage(ChatColor.RED.toString() + (TimeUnit.MILLISECONDS.toSeconds(timeLeft) - CooldownManager.DEFAULT_COOLDOWN) + " seconds before " + ue + " can place blocks again"); 
                 
             }
             
--- src/com/ActionCooldown/CooldownManager.java
+++ src/com/ActionCooldown/CooldownManager.java
@@ -2,27 +2,25 @@ package com.ActionCooldown;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 public class CooldownManager {
-	
 	
 	private static MainClass mainClass = MainClass.getInstance();
 	
 	public static long DEFAULT_COOLDOWN = Long.parseLong(mainClass.getConfig().getString("global-cooldown-seconds"));
 
-    private final Map<UUID, Long> cooldowns = new HashMap<>();
+    private final Map<String, Long> cooldowns = new HashMap<>();
 
-    public void setCooldown(UUID player, long time) {
+    public void setCooldown(String ue, long time) {
         if(time < 1) {
-            cooldowns.remove(player);
+            cooldowns.remove(ue);
         } else {
-            cooldowns.put(player, time);
+            cooldowns.put(ue, time);
         }
     }
 
-    public long getCooldown(UUID player){
-        return cooldowns.getOrDefault(player, (long) 0);
+    public long getCooldown(String ue){
+        return cooldowns.getOrDefault(ue, (long) 0);
     }
 
 }
