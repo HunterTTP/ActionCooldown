e97e57575ec41052d87b86f275189a1dc1f16179
HunterTTP <HunterTTP@gmail.com>
2022-05-22 10:41:07 -0400

    Refactored onAttack event handler to simplify the code

--- src/main/java/me/hunterttp/actioncooldown/ActionListener.java
+++ src/main/java/me/hunterttp/actioncooldown/ActionListener.java
@@ -143,75 +143,28 @@ public class ActionListener implements Listener {
     @EventHandler
     public void onAttack(EntityDamageByEntityEvent event) {
 
+        Player player = null;
+
         //set player
-        if (event.getDamager() instanceof Arrow){
+        if (event.getDamager() instanceof Arrow) {
 
             Arrow arrow = (Arrow) event.getDamager();
 
             if (arrow.getShooter() instanceof Player) {
 
-                Player player = (Player) arrow.getShooter();
-                String uniqueId = player.getUniqueId().toString();
-                String eventName = event.getEventName();
-                String playerEvent = uniqueId + eventName;
-
-                //Custom variables
-                String exemptMobs = ConfigSettings.attack_exempt_mobs;
-                String targetName = event.getEntity().toString();
-                String cdEnabled = ConfigSettings.attack_cd_enabled;
-                String[] exemptTargetArray = exemptMobs.toLowerCase().split(",");
-                long actionLimit = ConfigSettings.attack_cd_limit;
-                long cdDuration = ConfigSettings.attack_cd_duration;
-                boolean exemptTargetCheck = Arrays.asList(exemptTargetArray).contains(targetName);
-
-                //check if this module is enabled in the settings and if this particular target is exempt
-                if (cdEnabled == "true" && !exemptTargetCheck && actionLimit > 0) {
-
-                    //add (or start) a count to represent the player successfully executing the action
-                    actionTracker.addCount(playerEvent);
-
-                    //if total number of actions is less than the limit (config.yml)
-                    if (actionTracker.checkCount(playerEvent) < actionLimit) {
-
-                        //notify the user of events remaining (config.yml)
-                        actionTracker.notifyCount(player, eventName, actionLimit, playerEvent);
-
-                        //if total number of actions are equal to the limit
-                    } else if (actionTracker.checkCount(playerEvent) == actionLimit) {
-
-                        //start the cooldown timer which will reset the number of actions when done
-                        actionTracker.startTimer(player, eventName, playerEvent, cdDuration);
-
-                        //if enabled (config.yml) notify the user of events remaining
-                        actionTracker.notifyCount(player, eventName, actionLimit, playerEvent);
-
-                        //if action limit is reached and/or cooldown is not finished
-                    } else {
-
-                        //cancel the event
-                        event.setCancelled(true);
-
-                        //remove the count as the action was cancelled
-                        actionTracker.subtractCount(playerEvent);
-
-                        //notify player of the time remaining before the cooldown is complete
-                        actionTracker.notifyCooldown(player, playerEvent, cdDuration);
-
-                    }
-
-                    //if action limit is zero (config.yml)
-                } else if (actionLimit <= 0) {
-
-                    //notify player with a special message
-                    player.sendMessage(ChatColor.DARK_RED.toString() + "This action is not allowed");
-
-                }
+                player = (Player) arrow.getShooter();
 
             }
 
-        }else if(event.getDamager() instanceof Player){
+        } else if (event.getDamager() instanceof Player) {
 
-            Player player = (Player) event.getDamager();
+            player = (Player) event.getDamager();
+
+        }
+
+        //send message
+        if (player != null) {
+
             String uniqueId = player.getUniqueId().toString();
             String eventName = event.getEventName();
             String playerEvent = uniqueId + eventName;
