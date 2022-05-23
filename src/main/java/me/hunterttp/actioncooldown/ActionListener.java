eef9198bbf4dcfb3223d66c03504399821dd5f81
HunterTTP <HunterTTP@gmail.com>
2022-05-23 12:16:16 -0400

    Added cooldown control for dropping and picking up items

--- src/main/java/me/hunterttp/actioncooldown/ActionListener.java
+++ src/main/java/me/hunterttp/actioncooldown/ActionListener.java
@@ -7,7 +7,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import java.util.Arrays;
 
 public class ActionListener implements Listener {
@@ -26,11 +27,13 @@ public class ActionListener implements Listener {
         //Custom variables
         String exemptBlocks = ConfigSettings.block_place_exempt;
         String cdEnabled = ConfigSettings.block_place_cd_enabled;
-        String targetName = event.getBlockPlaced().getType().toString().toLowerCase();
+        String targetName = event.getBlockPlaced().getType().toString().toLowerCase().replace(" ","_");
         String[] exemptTargetArray = exemptBlocks.toLowerCase().split(",");
         long actionLimit = ConfigSettings.block_place_cd_limit;
         long cdDuration = ConfigSettings.block_place_cd_duration;
         boolean targetExemptCheck = Arrays.asList(exemptTargetArray).contains(targetName);
+
+        player.sendMessage(targetName);
 
         if(actionTracker.runChecks(player, eventName, playerEvent, cdEnabled, actionLimit, cdDuration, targetExemptCheck)){
 
@@ -51,7 +54,7 @@ public class ActionListener implements Listener {
         String playerEvent = uniqueId + eventName;
 
         //Custom variables
-        String targetName = event.getBlock().getType().toString().toLowerCase();
+        String targetName = event.getBlock().getType().toString().toLowerCase().replace(" ","_");
         String exemptBlocks = ConfigSettings.block_break_exempt;
         String cdEnabled = ConfigSettings.block_break_cd_enabled;
         String[] exemptTargetArray = exemptBlocks.toLowerCase().split(",");
@@ -99,7 +102,7 @@ public class ActionListener implements Listener {
 
             //Custom variables
             String exemptMobs = ConfigSettings.attack_exempt_mobs;
-            String targetName = event.getEntity().toString();
+            String targetName = event.getEntity().toString().toLowerCase().replace("craft","").replace(" ","_");
             String cdEnabled = ConfigSettings.attack_cd_enabled;
             String[] exemptTargetArray = exemptMobs.toLowerCase().split(",");
             long actionLimit = ConfigSettings.attack_cd_limit;
@@ -114,6 +117,66 @@ public class ActionListener implements Listener {
             }
 
         }
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+
+        //Constant variables
+        Player player = event.getPlayer();
+        String uniqueId = player.getUniqueId().toString();
+        String eventName = event.getEventName();
+        String playerEvent = uniqueId + eventName;
+
+        //Custom variables
+        String cdEnabled = ConfigSettings.drop_cd_enabled;
+        String exemptTargets = ConfigSettings.drop_exempt_mobs;
+        String targetName = event.getItemDrop().getName().toLowerCase().replace(" ","_");
+        String[] exemptTargetArray = exemptTargets.toLowerCase().split(",");
+        long actionLimit = ConfigSettings.drop_cd_limit;
+        long cdDuration = ConfigSettings.drop_cd_duration;
+        boolean targetExemptCheck = Arrays.asList(exemptTargetArray).contains(targetName);
+
+        if (actionTracker.runChecks(player, eventName, playerEvent, cdEnabled, actionLimit, cdDuration, targetExemptCheck)) {
+
+            //cancel the event
+            event.setCancelled(true);
+
+        }
+
+    }
+
+    @EventHandler
+    public void onPickup(EntityPickupItemEvent event) {
+
+        //set player
+        if (event.getEntity() instanceof Player) {
+
+            Player player = (Player) event.getEntity();
+
+            //Constant variables
+            String uniqueId = player.getUniqueId().toString();
+            String eventName = event.getEventName();
+            String playerEvent = uniqueId + eventName;
+
+            //Custom variables
+            String cdEnabled = ConfigSettings.pickup_cd_enabled;
+            String exemptTargets = ConfigSettings.pickup_exempt_mobs;
+            String targetName = event.getItem().getName().toLowerCase().replace(" ","_");
+            String[] exemptTargetArray = exemptTargets.toLowerCase().split(",");
+            long actionLimit = ConfigSettings.pickup_cd_limit;
+            long cdDuration = ConfigSettings.pickup_cd_duration;
+            boolean targetExemptCheck = Arrays.asList(exemptTargetArray).contains(targetName);
+
+            if(actionTracker.runChecks(player, eventName, playerEvent, cdEnabled, actionLimit, cdDuration, targetExemptCheck)){
+
+                //cancel the event
+                event.setCancelled(true);
+
+            }
+
+        }
+
     }
 
 }
--- src/main/java/me/hunterttp/actioncooldown/ConfigSettings.java
+++ src/main/java/me/hunterttp/actioncooldown/ConfigSettings.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class ConfigSettings {
 
-    private static MainClass mainClass = MainClass.getInstance();
+    private static final MainClass mainClass = MainClass.getInstance();
 
     public static String notify_chat_limit = mainClass.getConfig().getString("notify-chat-on-remaining-uses");
     public static String notify_chat_cd = mainClass.getConfig().getString("notify-chat-on-cooldown-completion");
@@ -24,5 +24,16 @@ public class ConfigSettings {
     public static long attack_cd_limit = Long.parseLong(Objects.requireNonNull(mainClass.getConfig().getString("attack-cooldown-actionlimit")));
     public static long attack_cd_duration = Long.parseLong(Objects.requireNonNull(mainClass.getConfig().getString("attack-cooldown-duration")));
     public static String attack_exempt_mobs = mainClass.getConfig().getString("attack-cooldown-exempt-mobs");
+
+    public static String drop_cd_enabled = mainClass.getConfig().getString("drop-item-cooldown-enabled");
+    public static long drop_cd_limit = Long.parseLong(Objects.requireNonNull(mainClass.getConfig().getString("drop-item-cooldown-actionlimit")));
+    public static long drop_cd_duration = Long.parseLong(Objects.requireNonNull(mainClass.getConfig().getString("drop-item-cooldown-duration")));
+    public static String drop_exempt_mobs = mainClass.getConfig().getString("drop-item-cooldown-exempt-items");
+
+    public static String pickup_cd_enabled = mainClass.getConfig().getString("pickup-item-cooldown-enabled");
+    public static long pickup_cd_limit = Long.parseLong(Objects.requireNonNull(mainClass.getConfig().getString("pickup-item-cooldown-actionlimit")));
+    public static long pickup_cd_duration = Long.parseLong(Objects.requireNonNull(mainClass.getConfig().getString("pickup-item-cooldown-duration")));
+    public static String pickup_exempt_mobs = mainClass.getConfig().getString("pickup-item-cooldown-exempt-items");
+
 
 }
