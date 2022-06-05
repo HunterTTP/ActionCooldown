5452b183b8103179a4196d6b3f80f8772d549f4c
HunterTTP <HunterTTP@gmail.com>
2022-06-05 12:34:45 -0400

    All settings can now be adjusted through commands

--- src/main/java/me/hunterttp/actioncooldown/CommandListener.java
+++ src/main/java/me/hunterttp/actioncooldown/CommandListener.java
@@ -4,7 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import java.util.Objects;
+import static java.lang.Integer.parseInt;
 
 public class CommandListener implements CommandExecutor {
 
@@ -19,47 +19,13 @@ public class CommandListener implements CommandExecutor {
 
                 sender.sendMessage("ActionCooldown is installed!");
 
-            }else if(Objects.equals(args[0], "chat-notify-count")) {
+            }else if(args.length == 1 && mainClass.getConfig().contains(args[0])) {
 
-                if (args.length == 2 && args[1] != null) {
+                sender.sendMessage(args[0] + " is currently set to " + mainClass.getConfig().getString(args[0]));
 
-                    mainClass.getConfig().set("notify-chat-on-remaining-uses", Boolean.parseBoolean(args[1]));
-                    mainClass.saveConfig();
-                    sender.sendMessage(args[0] + " has been set to " + mainClass.getConfig().getBoolean("notify-chat-on-remaining-uses"));
+            }else if(args.length == 2 && args[1] != null && mainClass.getConfig().contains(args[0])) {
 
-                } else {
-
-                    sender.sendMessage(args[0] + " is currently set to " + mainClass.getConfig().getBoolean("notify-chat-on-remaining-uses"));
-
-                }
-
-            }else if(Objects.equals(args[0], "chat-notify-cd")) {
-
-                if (args.length == 2 && args[1] != null) {
-
-                    mainClass.getConfig().set("notify-chat-on-cooldown-completion", Boolean.parseBoolean(args[1]));
-                    mainClass.saveConfig();
-                    sender.sendMessage(args[0] + " has been set to " + mainClass.getConfig().getBoolean("notify-chat-on-cooldown-completion"));
-
-                } else {
-
-                    sender.sendMessage(args[0] + " is currently set to " + mainClass.getConfig().getBoolean("notify-chat-on-remaining-uses"));
-
-                }
-
-            }else if(Objects.equals(args[0], "sound-notify-cd")) {
-
-                if (args.length == 2 && args[1] != null) {
-
-                    mainClass.getConfig().set("play-sound-on-cooldown-completion", Boolean.parseBoolean(args[1]));
-                    mainClass.saveConfig();
-                    sender.sendMessage(args[0] + " has been set to " + mainClass.getConfig().getBoolean("play-sound-on-cooldown-completion"));
-
-                } else {
-
-                    sender.sendMessage(args[0] + " is currently set to " + mainClass.getConfig().getBoolean("play-sound-on-cooldown-completion"));
-
-                }
+                updateConfig(sender, args);
 
             }else{
 
@@ -69,8 +35,37 @@ public class CommandListener implements CommandExecutor {
 
         }
 
-        // If the player (or console) uses our command correct, we can return true
         return true;
+
+    }
+
+    public void updateConfig(CommandSender sender, String[] args){
+
+        String setting = args[0];
+
+        if(args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("false")) {
+
+            boolean newValue = Boolean.parseBoolean(args[1]);
+            mainClass.getConfig().set(setting, newValue);
+            mainClass.saveConfig();
+            sender.sendMessage(setting + " has been set to " + newValue);
+
+        }else if(args[1].matches("[0-9]+")){
+
+            int newValue = parseInt(args[1]);
+            mainClass.getConfig().set(setting, newValue);
+            mainClass.saveConfig();
+            sender.sendMessage(setting + " has been set to " + newValue);
+
+        }else{
+
+            //need to update this to handle adding entities to exemptions in config.yml
+            String newValue = args[1];
+            mainClass.getConfig().set(setting, newValue);
+            mainClass.saveConfig();
+            sender.sendMessage(setting + " has been set to " + newValue);
+
+        }
 
     }
 
